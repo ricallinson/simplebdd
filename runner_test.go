f1e472874d92cbc251809166e93e92701521e016
Ric Allinson <capecodehq@yahoo.co.uk>
2015-07-06 17:24:09 -0700

    Added first test

---
+++ runner_test.go
@@ -0,0 +1,17 @@
+package simplebdd
+
+import (
+    "testing"
+)
+
+func TestRunner(t *testing.T) {
+
+    Describe("Describe() and It()", func() {
+
+        It("should return true after being called", func() {
+            AssertEqual(true, true)
+        })
+    })
+
+    Report(t)
+}
